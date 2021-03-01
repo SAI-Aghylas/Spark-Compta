353818bf61a82763e43f180ada50b96700793489
aghylassai <aghilassai@gmail.com>
2021-03-01 11:39:08 +0100

    Casting types and Filling empty columns

--- src/main/scala/Pretraincsv/Csvreader.scala
+++ src/main/scala/Pretraincsv/Csvreader.scala
@@ -1,9 +1,25 @@
 package Pretraincsv
 
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 class Csvreader(spark: SparkSession) {
   def readcsv(path:String): DataFrame ={
+//    val schema = new StructType()
+//      schema.add("COMPTE_COMPTABLE",IntegerType,true)
+//      .add("DATE_MVT",DateType,true)
+//      .add("PIECE_COMPTABLE",StringType,true)
+//      .add("MONTANT",FloatType,true)
+//      .add("SENS",StringType,true)
+//      .add("LIBELLE_FACTURE",StringType,true)
+//      .add("DATE_REGLEMENT",DateType,true)
+//      .add("DATE_ECHEANCE",DateType,true)
+//      .add("HICTSA",StringType,true)
+//    spark.read.format("csv")
+//      .option("header", "true")
+//      .option("delimiter", ";")
+//      .schema(schema)
+//      .load(path)
     spark.read
       .option("header", "true")
       .option("delimiter", ";")
--- src/main/scala/Pretraincsv/pretraitementCsv.scala
+++ src/main/scala/Pretraincsv/pretraitementCsv.scala
@@ -1,13 +1,16 @@
 package Pretraincsv
-import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.types.{FloatType, IntegerType, StringType, StructType}
+import org.apache.spark.sql.{Column, Row, SparkSession}
+import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
 import Pretraincsv.Csvreader
 
 
+
 object pretraitementCsv {
+
   val PATH_TO_FILE="./src/main/Data-files/lettrage et ecriture comptable.csv"
+
   def main(args: Array[String]): Unit = {
     implicit val spark=SparkSession.builder()
                           .appName("Reading_csv_file")
@@ -15,13 +18,36 @@ object pretraitementCsv {
                           .getOrCreate()
     val csvreader=Csvreader(spark)
     val csvFile= csvreader.readcsv(PATH_TO_FILE)
-    //csvreader.showcsv(csvFile)
-    csvFile
-      .withColumn("DATE_MVT", csvFile("DATE_MVT").cast(StringType))
-      .withColumn("MONTANT",csvFile("MONTANT").cast(FloatType))
-      .select(csvFile("COMPTE_COMPTABLE").alias("Compte_Comptable"))
-      .select(csvFile("PIECE_COMPTABLE").alias("Piece_Comptable"))
-      .withColumn("type_compte",csvFile("COMPTE_COMPTABLE").substr(1,3) )
-      .printSchema()
+
+    def replace_empty(ColIn: Column, ReplaceVal: Any): Column = {
+     return( when(ColIn.isNull,lit(ReplaceVal)).otherwise(ColIn) )
+    }
+
+    // Transform column Types
+    val csvUpdateTypes=csvFile
+      .withColumn("DATE_MVT", to_date(csvFile("DATE_MVT").cast(StringType),"yyyymmdd"))
+      .withColumn("MONTANT",regexp_replace(csvFile("Montant"),",",".").cast(DoubleType))
+      .withColumn("LIBELLE_FACTURE",split(csvFile("LIBELLE_FACTURE")," ")(0))
+      .withColumn("DATE_REGLEMENT",to_date(csvFile("DATE_REGLEMENT"),"yyyymmdd"))
+      .withColumn("DATE_ECHEANCE",to_date(csvFile("DATE_ECHEANCE"),"yyyymmdd"))
+      .withColumn("COMPTE_COMPTABLE",col("COMPTE_COMPTABLE").cast(LongType))
+
+    //Fill NUll or Empty values
+    val csvFilled=csvUpdateTypes
+      .withColumn("DATE_ECHEANCE",replace_empty(col("DATE_ECHEANCE"),current_date()))
+      .withColumn("DATE_REGLEMENT",replace_empty(col("DATE_REGLEMENT"),current_date()))
+      .withColumn("PIECE_COMPTABLE",regexp_replace(col("PIECE_COMPTABLE"),"      ","AUCUN"))
+      .withColumn("HICTSA",regexp_replace(col("HICTSA")," ","X"))
+
+
+
+    csvFilled
+      .withColumn("TYPE_COMPTE",substring(col("COMPTE_COMPTABLE"),1,3))
+  //  csvFilled.groupBy("HICTSA").agg(length(col("HICTSA"))).distinct().show()
+//    val tst =csvFilled.withColumn("PIECE_COMPTABLE_FILLED",replace_empty(col("PIECE_COMPTABLE"),"X"))
+
+//      tst.groupBy("PIECE_COMPTABLE").agg(col("PIECE_COMPTABLE"),length(col("PIECE_COMPTABLE"))).distinct().sort("PIECE_COMPTABLE").show(100)
+//    tst.select(count("PIECE_COMPTABLE_FILLED")).distinct()
+
   }
 }
